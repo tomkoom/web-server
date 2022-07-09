e86106d50a933ca0c1664be420823a516fcc2767
tomkoom <tomash.sugint@gmail.com>
2022-07-10 01:37:04 +0300

    feat: add templating

--- src/app.js
+++ src/app.js
@@ -1,13 +1,42 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 console.log(__dirname);
 console.log(path.join(__dirname, "../public"));
 
 const app = express();
-const pubDirPath = path.join(__dirname, "../public");
 
+// paths
+const pubDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// static dir
 app.use(express.static(pubDirPath));
+
+app.get("", (req, res) => {
+	res.render("index", {
+		title: "Weather App",
+	});
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "About",
+	});
+});
+
+app.get("/help", (req, res) => {
+	res.render("help", {
+		title: "Help",
+	});
+});
 
 app.get("/weather", (req, res) => {
 	res.send({
